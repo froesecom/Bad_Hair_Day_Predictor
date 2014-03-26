96966bb4346146baca2b03b2ea9b713d1181ed45
Erik Froese <froesecom@gmail.com>
2014-03-26 23:10:40 +1100

    more styling, but getting tired, so thought I best commit

--- app/models/hairstyle.rb
+++ app/models/hairstyle.rb
@@ -214,7 +214,7 @@ class Hairstyle < ActiveRecord::Base
         when 20.0..30.0
           bad_hair_prediction = ["Good day for hair", "Hot diddly dog!", "good", "/assets/good.jpg"]
         when 30.0..40.0
-          bad_hair_prediction = ["Average day for hair", "Did you say average!", "average", "/assets/average.jpg"]
+          bad_hair_prediction = ["Average day for hair", "Who said average!", "average", "/assets/average.jpg"]
         when 40.0..50.0
           bad_hair_prediction = ["Below average day for hair", "You have got to be fucking kidding me.", "below_average", "/assets/below_average.jpg"]
         when 50.0..60.0
