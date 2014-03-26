c2840abba6fbb4e89b712cc42ccc05ddfdfe5096
Erik Froese <froesecom@gmail.com>
2014-03-27 09:59:04 +1100

    User edit now working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,8 +52,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find params[:id]
+    
   end
+  
   def update
+    user = User.find params[:id]
+    user.update_attributes params[:user]
+    redirect_to user_path(@current_user.id)
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     links = ''
     if @current_user
       
-      links += "#{link_to('Your custom page ' + @current_user.name,  user_path(@current_user.id)) } #{link_to('Log out ' , login_path, :method => :delete, :confirm => 'Really log out?') } 
+      links += "#{link_to(@current_user.name  + "'s custom page"  ,  user_path(@current_user.id)) } #{link_to('Log out ' , login_path, :method => :delete, :confirm => 'Really log out?') } 
       "
     else
       links += "#{ link_to('Register', new_user_path) } #{ link_to('Login', login_path) }"
