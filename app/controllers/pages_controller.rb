17f177faa71a9d2fc64499c01f996a0b989775ce
Erik Froese <froesecom@gmail.com>
2014-03-27 17:01:07 +1100

    ERROR CHECKING FOR API ADDED. Works, unbelievably

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,25 +5,29 @@ class PagesController < ApplicationController
     @hairstyle_attributes = Hairstyle.current_attributes
   end
 
-  def results
+  def results 
     weather = Hairstyle.weather(params[:user][:country].gsub(" ", "_"), params[:city].gsub(" ", "_"))
-    @city = weather[:city]
-    @country = weather[:country]
-    @humidity = weather[:humidity]
-    @temp = weather[:temp]
-    @wind = weather[:wind]
-    @pop = weather[:pop]
-    @weather_icon = weather[:weather_icon]
-    @wunderground_logo = weather[:wunderground_logo]
-    # This monster passes things into the prediction algorithem. It needs to be refactored.
-    # The two ones at the end are defaul humidty and hair thickness multipliers, which I used for logged in users.
-    prediction_results = Hairstyle.prediction(params[:length], params[:curliness], params[:hygiene], @humidity, @wind, @pop, params[:modifications], 1, 1)
-    @bad_hair_prediction = prediction_results[:prediction].first
-    @message = prediction_results[:prediction][1]
-    @div_id = prediction_results[:prediction][2]
-    @background_url = prediction_results[:prediction][3]
-    @reasons = prediction_results[:reasons]
     
+    if weather.class == Hash 
+      @city = weather[:city]
+      @country = weather[:country]
+      @humidity = weather[:humidity]
+      @temp = weather[:temp]
+      @wind = weather[:wind]
+      @pop = weather[:pop]
+      @weather_icon = weather[:weather_icon]
+      @wunderground_logo = weather[:wunderground_logo]
+      # This monster passes things into the prediction algorithem. It needs to be refactored.
+      # The two ones at the end are defaul humidty and hair thickness multipliers, which I used for logged in users.
+      prediction_results = Hairstyle.prediction(params[:length], params[:curliness], params[:hygiene], @humidity, @wind, @pop, params[:modifications], 1, 1)
+      @bad_hair_prediction = prediction_results[:prediction].first
+      @message = prediction_results[:prediction][1]
+      @div_id = prediction_results[:prediction][2]
+      @background_url = prediction_results[:prediction][3]
+      @reasons = prediction_results[:reasons]
+    else
+      @error = weather
+    end
   end
   def error
   end
--- app/models/hairstyle.rb
+++ app/models/hairstyle.rb
@@ -79,17 +79,20 @@ class Hairstyle < ActiveRecord::Base
     response = HTTParty.get(url)
     json_string = JSON(response)
     parsed_json = JSON.parse(json_string)
-    {
-    :city => parsed_json['location']['city'],
-    :country => parsed_json['location']['country_name'],
-    :humidity => parsed_json["forecast"]["simpleforecast"]["forecastday"].first['avehumidity'],
-    :temp => parsed_json["forecast"]["simpleforecast"]["forecastday"].first['high']['celsius'],
-    :wind => @humidity = parsed_json["forecast"]["simpleforecast"]["forecastday"].first['avewind']['kph'],
-    :pop => parsed_json["forecast"]["simpleforecast"]["forecastday"].first['pop'],
-    :weather_icon => parsed_json["forecast"]["simpleforecast"]["forecastday"].first['icon_url'],
-    :wunderground_logo => parsed_json['current_observation']['image']['url']
-  }
-
+    if parsed_json['response']['error']
+      error = "Sorry, we don't recognise that city or country."
+    else
+      {
+      :city => parsed_json['location']['city'],
+      :country => parsed_json['location']['country_name'],
+      :humidity => parsed_json["forecast"]["simpleforecast"]["forecastday"].first['avehumidity'],
+      :temp => parsed_json["forecast"]["simpleforecast"]["forecastday"].first['high']['celsius'],
+      :wind => @humidity = parsed_json["forecast"]["simpleforecast"]["forecastday"].first['avewind']['kph'],
+      :pop => parsed_json["forecast"]["simpleforecast"]["forecastday"].first['pop'],
+      :weather_icon => parsed_json["forecast"]["simpleforecast"]["forecastday"].first['icon_url'],
+      :wunderground_logo => parsed_json['current_observation']['image']['url']
+    }
+    end
   end
 
   def self.prediction(length_params, curliness_params, hygiene_params, humidity_params, wind_params, pop_params, modification_params, hum_sus, thickness_sus)
