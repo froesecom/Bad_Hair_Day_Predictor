cf180fa250f0537671bb359f24d5b60f64ced98d
Erik Froese <froesecom@gmail.com>
2014-03-27 12:00:21 +1100

    trying to fix email related files in production environmenent

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,17 @@ BadHair::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
-  config.assets.debug = true
+
+  # Send deprecation notices to registered listeners.
+  config.active_support.deprecation = :notify
+
+  # Disable automatic flushing of the log to improve performance.
+  # config.autoflush_log = false
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
+
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
@@ -79,13 +89,4 @@ BadHair::Application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
-end
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
-
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
 end
